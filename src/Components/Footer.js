9b96d16fd0d2b568ac4e5340f16419ae2e8cd270
Lincoln Wasden <lincolndwasden@gmail.com>
2022-04-26 11:11:28 -0600

    changes to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="Lincoln W." href="https://lincolnwasden.github.io/">
+                Lincoln W.
               </a>
             </li>
           </ul>
